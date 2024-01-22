b4a4a75a601e7fb0a8cca3cfadb601638fa910aa
NguyenVanDuongTDH <154052803+NguyenVanDuongTDH@users.noreply.github.com>
2024-01-22 22:58:19 +0700

    Update state_view_widget.dart

--- lib/state_view_widget.dart
+++ lib/state_view_widget.dart
@@ -6,37 +6,21 @@ import 'package:flutter/material.dart';
 abstract class StateViewWidget extends StatefulWidget {
   StateViewWidget({super.key});
   State? _state;
-
-  void setState(void Function() fn) => _state?.setState(() {
-        fn();
-      });
-  void reBuild() => _state?.setState(() {});
-
-  @override
-  State createState() {
-    return view();
-  }
-
-  @factory
-  @protected
-  State view();
+  void reBuild() => _state!.setState(() {});
 }
 
-abstract class ViewState<T extends StateViewWidget> extends State {
-  T? _model;
-  T get model => _model!;
-
+abstract class StateView<T extends StateViewWidget> extends State {
+  T get model => widget as T;
   @override
   void initState() {
     super.initState();
-    _model = widget as T?;
-    _model?._state = this;
+    model._state = this;
   }
 
   @override
   void dispose() {
-    _model?._state = null;
-    _model = null;
+    if (model._state != null) model._state = null;
     super.dispose();
   }
 }
+
